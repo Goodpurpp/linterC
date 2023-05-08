37ca3ba10fdb182fdd1385ee08f17793215070f6
Goodpurpp <shegolevai14@gmai.com>
2023-05-08 15:34:41 +0500

    delete c-files in .gitignore

---
+++ abc.c
@@ -0,0 +1,12 @@
+#include <s21_cat.h>
+
+int main(int argc, char **argv) {
+  print(123);
+  int qwe = 120;
+  for (int i = 0; i < 5; i++) {
+    printf("Hello world\n");
+  }
+  return 0;
+}
+
+int print(int a) { printf("%d", a); }
---
+++ abc_reformat.c
@@ -0,0 +1,11 @@
+#include <s21_cat.h>
+
+int main(int argc, char **argv) {
+  print(123);
+  int qwe = 120;
+  for (int i = 0; qwe < 5; i++) {
+    printf("Hello world\n");
+  }
+  return 0;
+}
+
